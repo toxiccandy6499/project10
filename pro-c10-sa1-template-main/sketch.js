ddda3d866ce9fd3c00c9a45e4548fb32ea5326e3
toxiccandy6499 <87265387+toxiccandy6499@users.noreply.github.com>
2021-07-17 13:48:27 +0530

    Add files via upload

---
+++ pro-c10-sa1-template-main/sketch.js
@@ -0,0 +1,19 @@
+var weight = [35,38,40,45];
+
+
+function setup() 
+{
+  createCanvas(400,400);
+
+var sum = weight[0] + weight[1] + weight[2]  + weight[3];
+var avg = sum / weight.length;
+console.log(avg)
+}
+
+function draw() 
+{
+background(51);
+
+}
+
+
